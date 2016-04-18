d3fa5a0518e784d8b2f140e11a4b231ce29bd742
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-04-18 20:20:53 +0200

    added the example code for post request handling.

--- src/main/webhook/webhook-post.js
+++ src/main/webhook/webhook-post.js
@@ -1,3 +1,5 @@
+var request = require("request")
+
 (function(){
 
 	var webhookPost = function (req, res) {
@@ -10,11 +12,35 @@
 			if (event.message && event.message.text) {
 				text = event.message.text;
 				// Handle a text message from this sender
-				console.log("received text: " + text)
+				console.log("received text: " + text);
+				sendTextMessage(sender, text);
 			}
 		}
 		res.sendStatus(200);
 	};
+
+	var token = "EAAWdYsStVAoBAFYgMQsmCDYZCXYq1Rb52tMlGSuHq7bFOOAX6yQmD1YZA2yzJ0KZCqpl6n0j5n6a7iG9F0ipDMiAapw7RjHqENARUy9FaAZAUpDZCZAEUVmW6ph2gxRyl3KRaceFPjvDC7KfCPpJDgdumZAMMh21E4wbSZBPL4tcfgZDZD";
+
+	function sendTextMessage(sender, text) {
+		var messageData = {
+			text: text
+		};
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token:token},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData
+			}
+		}, function(error, response, body) {
+			if (error) {
+				console.log('Error sending message: ', error);
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error);
+			}
+		});
+	}
 
 	module.exports = webhookPost;
 
