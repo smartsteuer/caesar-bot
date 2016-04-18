a8f677c0bbe353eb248f4a748d44370716b5cced
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-04-18 20:25:35 +0200

    had to remove function wrapper.

--- src/main/webhook/webhook-post.js
+++ src/main/webhook/webhook-post.js
@@ -1,47 +1,44 @@
-var request = require("request")
+var request = require("request");
 
-(function(){
 
-	var webhookPost = function (req, res) {
-		var messaging_events = req.body.entry[0].messaging;
-		var event, sender,i;
+var webhookPost = function (req, res) {
+	var messaging_events = req.body.entry[0].messaging;
+	var event, sender,i;
 
-		for (i = 0; i < messaging_events.length; i++) {
-			event = req.body.entry[0].messaging[i];
-			sender = event.sender.id;
-			if (event.message && event.message.text) {
-				text = event.message.text;
-				// Handle a text message from this sender
-				console.log("received text: " + text);
-				sendTextMessage(sender, text);
-			}
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i];
+		sender = event.sender.id;
+		if (event.message && event.message.text) {
+			text = event.message.text;
+			// Handle a text message from this sender
+			console.log("received text: " + text);
+			sendTextMessage(sender, text);
 		}
-		res.sendStatus(200);
-	};
-
-	var token = "EAAWdYsStVAoBAFYgMQsmCDYZCXYq1Rb52tMlGSuHq7bFOOAX6yQmD1YZA2yzJ0KZCqpl6n0j5n6a7iG9F0ipDMiAapw7RjHqENARUy9FaAZAUpDZCZAEUVmW6ph2gxRyl3KRaceFPjvDC7KfCPpJDgdumZAMMh21E4wbSZBPL4tcfgZDZD";
-
-	function sendTextMessage(sender, text) {
-		var messageData = {
-			text: text
-		};
-		request({
-			url: 'https://graph.facebook.com/v2.6/me/messages',
-			qs: {access_token:token},
-			method: 'POST',
-			json: {
-				recipient: {id:sender},
-				message: messageData
-			}
-		}, function(error, response, body) {
-			if (error) {
-				console.log('Error sending message: ', error);
-			} else if (response.body.error) {
-				console.log('Error: ', response.body.error);
-			}
-		});
 	}
+	res.sendStatus(200);
+};
 
-	module.exports = webhookPost;
+var token = "EAAWdYsStVAoBAFYgMQsmCDYZCXYq1Rb52tMlGSuHq7bFOOAX6yQmD1YZA2yzJ0KZCqpl6n0j5n6a7iG9F0ipDMiAapw7RjHqENARUy9FaAZAUpDZCZAEUVmW6ph2gxRyl3KRaceFPjvDC7KfCPpJDgdumZAMMh21E4wbSZBPL4tcfgZDZD";
 
-})();
+function sendTextMessage(sender, text) {
+	var messageData = {
+		text: text
+	};
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+}
+
+module.exports = webhookPost;
