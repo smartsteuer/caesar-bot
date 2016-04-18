0b790196226ea1822907f7cdeb5a2ee36b19dbec
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-04-18 18:44:17 +0200

    Hack-Deploy

--- src/main/webhook/webhook-post.js
+++ src/main/webhook/webhook-post.js
@@ -1,6 +1,21 @@
+(function(){
 
- var webhookPost = function(req, res) {
-	 console.log('message received!')
-};
+	var webhookPost = function (req, res) {
+		var messaging_events = req.body.entry[0].messaging;
+		var event, sender;
 
-module.exports = webhookPost;
+		for (i = 0; i < messaging_events.length; i++) {
+			event = req.body.entry[0].messaging[i];
+			sender = event.sender.id;
+			if (event.message && event.message.text) {
+				text = event.message.text;
+				// Handle a text message from this sender
+				console.log("received text: " + text)
+			}
+		}
+		res.sendStatus(200);
+	};
+
+	module.exports = webhookPost;
+
+})();
