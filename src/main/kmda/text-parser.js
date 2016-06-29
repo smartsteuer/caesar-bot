0356df3f34a295eb00a20d9fc6cf600a0562289b
Eike Hirsch <eike.hirsch@smartsteuer.de>
2016-06-29 09:34:45 +0200

    cleaning up.
    removes artifacts from our steuermann bot which are of no use in the scenario.

--- src/main/kmda/text-parser.js
+++
@@ -1,9 +0,0 @@
-const matcher = /(\w*?) (von der Steuer absetzen|absetzen|abschreiben)/i;
-
-module.exports = (text) => {
-	let result = matcher.exec(text);
-	if( null === result ) {
-		return null;
-	}
-    return result[1];
-};
